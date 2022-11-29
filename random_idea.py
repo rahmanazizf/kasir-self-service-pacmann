f90450b30ca1109d6c5202d648fe642d73e8bd3d
Rahman Aziz <rahmanazizf@gmail.com>
2022-11-29 17:48:02 +0700

    semua method done, kecuali check order

--- random_idea.py
+++ random_idea.py
@@ -25,7 +25,48 @@ class Transaction:
         else:
             print('Data yang Anda maksud tidak tersedia')
 
+
+    def update_item_price(self, nama_item, harga_per_item_updated):
+        if nama_item in self.item['Nama Item']:
+            idx = self['Nama Item'].index(nama_item)
+            self.item['Harga per Item'][idx] = harga_per_item_updated
+        else:
+            print("Data yang Anda maksud tidak tersedia")
+
+    def delete_item(self, nama_item):
+        if nama_item in self.item['Nama Item']:
+            idx = self.item['Nama Item'].index(nama_item)
+
+            removed_element = []
+            for data in self.item.values():
+                removed_element.append(data.pop(idx))
             
+            print("Data berikut telah berhasil dihapus.")
+            print(removed_element)
+
+    def reset_transaction(self):
+        self.item.clear()
+        print("Semua transaksi telah dihapus.")
+
+
+    def check_order(self):
+        pass
+
+    def total_price(self):
+        total = 0
+
+        for n_item, price in zip(self.item['Jumlah'], self.item['Harga per Item']):
+            total += n_item * price
+        
+        if total > 400_000:
+            total = total * (1 - 0.1)
+        elif total > 300_000:
+            total = total * (1 - 0.08)
+        elif total > 200_000:
+            total = total * (1 - 0.05)
+        
+        print(f"Harga total: {total}")
+                    
 
 tsx_123 = Transaction()
 
