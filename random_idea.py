417d070552593e33e9c53a2b0d923d53b37b05a7
Rahman Aziz <rahmanazizf@gmail.com>
2022-11-29 09:38:59 +0700

    membuat fungsi add_item dan update_item_name

--- random_idea.py
+++ random_idea.py
@@ -1,26 +1,35 @@
-class Cashier:
+from tabulate import tabulate
 
-    item = {}
+class Transaction:
 
-    def add_item():
-        item_name = input("Item name: ")
-        number_of_item = int(input("Number of item: "))
-        price_per_item = int(input("Price per item: "))
+    def __init__(self):
+        self.item = {'Nama Item': [], 'Jumlah': [], 'Harga per Item': []}
 
-        if item_name in Cashier.item:
-            print("Item name already exist in database.\n")
-            add_item()
-        else:
-            Cashier.item[item_name] = {'number of item': number_of_item,
-                                        'price per item': price_per_item}
-    
-    def show_all():
-        # show all item with tabulate
+    def add_item(self, nama_item, jumlah_item, harga_per_item):
+        try:
+            harga_per_item = float(harga_per_item)
 
-    def delete_item():
-        print("you are about to delete an item.\n")
-        item_name = input("Please input an item name: ")
-        del Cashier.item[item_name]
+            if (type(nama_item) == str) & (type(jumlah_item) == int) & (type(harga_per_item) == float):
+                self.item['Nama Item'].append(nama_item)
+                self.item['Jumlah'].append(jumlah_item)
+                self.item['Harga per Item'].append(harga_per_item)
 
-    
+        except Exception as exc:
+            raise ValueError("Data yang Anda masukkan tidak valid") from exc
         
+    def update_item_name(self, nama_item, nama_item_updated):
+        
+        if nama_item in self.item['Nama Item']:
+            idx = self.item['Nama Item'].index(nama_item)
+            self.item['Nama Item'][idx] = nama_item_updated
+        else:
+            print('Data yang Anda maksud tidak tersedia')
+
+            
+
+tsx_123 = Transaction()
+
+tsx_123.add_item('Roti', 1, 7000)
+tsx_123.update_item_name('Roti', 'Sari Roti')
+print(tsx_123.item)
+
