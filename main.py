c5aab3259fe452ea3a52e4cc461c4d6916238cb2
Rahman Aziz <rahmanazizf@gmail.com>
2023-01-17 15:16:43 +0700

    perbaikan fungsi-fungsi di dalam kelas dan penambahan docstring

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import numpy as np
 from transaction import Transaction
 
 tsx123 = Transaction()
-cmd_trx = {1: tsx123.add_item, 2: tsx123.update_item_name,
-            3: tsx123.update_item_price, 4: tsx123.delete_item, 
+cmd_trx = {1: tsx123.add_item, 
+            2: tsx123.update_item_name,
+            3: tsx123.update_item_price, 
+            4: tsx123.delete_item, 
             5: tsx123.reset_transaction, 
-            6: tsx123.check_order, 7: tsx123.total_price, 8: quit}
+            6: tsx123.check_order, 
+            7: tsx123.total_price, 
+            8: quit}
 
 print('{:*^50}'.format('Self-Service Pac-Cashier'))
 
 def menu():
-    print("\n")
+    '''
+    Performing a particular action entered by user.
+    Parameters: None
+    Input: Choice of action to perform (int)
+    Return: None
+    '''
     print("="*28)
-    print("Silakan ketikkan angka pada menu berikut\n")
+    print("Silakan ketikkan angka pada menu berikut.")
     print('''
     1. Tambahkan item belanja
     2. Ubah nama item terdaftar
@@ -24,7 +33,13 @@ def menu():
     7. Harga total
     8. Keluar\n
     ''')
-    cmd_number = int(input("Perintah Anda: "))
+
+    try:
+        cmd_number = int(input("Perintah Anda: "))
+    except ValueError:
+        # jika perintah yang dimasukkan bukan integer maka tampilkan pesan dan kembali ke menu
+        print("Perintah yang Anda masukkan tidak valid")
+        menu()
 
     cmd_trx[cmd_number]()
 
--- transaction.py
+++ transaction.py
@@ -1,27 +1,51 @@
 from tabulate import tabulate
+from money import Money
+
 
 class Transaction:
+    '''
+    '''
 
     def __init__(self):
-        self.item = {'Nama Item': [], 'Jumlah': [], 'Harga per Item': [], 
-                    "Harga": []}
+        self.item = {'Nama Item': [], 'Jumlah': [], 'Harga per Item': [],
+                     "Harga": []}
 
     def add_item(self):
+        '''
+        Adding an item into the order list.
+        Params: None
+        Inputs:\n 
+            Item name (str)
+            Number of item to buy (int)
+            Price per item (Money)
+        Return: None
+        '''
         try:
+            print(30*'*')
             print("Menambahkan item belanja")
             nama_item = input("Masukkan nama item belanja: ")
             jumlah_item = int(input("Masukkan jumlah item: "))
-            harga_per_item = float(input("Harga per item: "))
+            harga_per_item = Money(input("Harga per item: "), 'IDR')
 
             self.item['Nama Item'].append(nama_item)
             self.item['Jumlah'].append(jumlah_item)
             self.item['Harga per Item'].append(harga_per_item)
-            self.item['Harga'].append(jumlah_item * harga_per_item)
+            self.item['Harga'].append(harga_per_item * jumlah_item)
 
-        except Exception as exc:
-            raise ValueError("Data yang Anda masukkan tidak valid") from exc
-        
+            print('**Pesanan berhasil ditambahkan!**')
+        except ValueError:
+            print("Data yang Anda masukkan tidak valid.")
+            self.add_item()
+
     def update_item_name(self):
+        '''
+        Updating an existing item name in the order list.
+        Params: None
+        Inputs:\n
+            Existing item name (str)
+            New item name (str)
+        Return: None
+        '''
         print("Mengubah nama item belanja")
 
         nama_item = input("Masukkan nama yang telah terdaftar sebelumnya: ")
@@ -33,22 +57,39 @@ class Transaction:
         else:
             print('Data yang Anda maksud tidak tersedia')
             Transaction.update_item_name(self)
-
+            self.update_item_name()
 
     def update_item_price(self):
+        '''
+        Updating an existing item price in the order list.
+        Params: None
+        Inputs:
+            Item name (str)
+            Price per item updated (Money)
+        Return: None
+        '''
         print("Mengubah harga item belanja")
 
         nama_item = input("Masukkan nama item: ")
-        harga_per_item_updated = float(input("Masukkan harga item baru: "))
+        harga_per_item_updated = Money(
+            input("Masukkan harga item baru: "), 'IDR')
 
         if nama_item in self.item['Nama Item']:
             idx = self.item['Nama Item'].index(nama_item)
             self.item['Harga per Item'][idx] = harga_per_item_updated
-            self.item['Harga'][idx] = harga_per_item_updated * self.item['Jumlah'][idx]
+            self.item['Harga'][idx] = harga_per_item_updated * \
+                self.item['Jumlah'][idx]
         else:
             print("Data yang Anda maksud tidak tersedia")
 
     def delete_item(self):
+        '''
+        Deleting an existing item in the order list.
+        Params: None
+        Inputs:
+            Item name (str)
+        Return: None
+        '''
         print("Manghapus item belanja")
 
         nama_item = input("Masukkan nama item: ")
@@ -56,36 +97,49 @@ class Transaction:
         if nama_item in self.item['Nama Item']:
             idx = self.item['Nama Item'].index(nama_item)
 
-            removed_element = []
-            for data in self.item.values():
-                removed_element.append(data.pop(idx))
-            
-            print("Data berikut telah berhasil dihapus.")
-            print(removed_element)
+            removed_element = [data[idx] for data in self.item.values()]
+            headers = [header for header in self.item.keys()]
+            print(tabulate(removed_element, headers=headers, tablefmt='github'))
+
+            answer = input("Data berikut akan dihapus. Lanjutkan? Y/N\n")
+            if answer in ('Y', 'y'):
+                for data in self.item.values():
+                    data.pop(idx)
 
     def reset_transaction(self):
-        print("Menghapus seluruh transaksi belanja")
-        for data in self.item.values():
-            del data[:]
-        print("Semua transaksi telah dihapus")
-        print(tabulate(self.item, headers='keys', tablefmt='github'))
+        '''
+        Delete all existing item in the order list.
+        Return: None
+        '''
+        ans = input("Seluruh transaksi akan dihapus. Lanjutkan? Y/N\n")
+        if ans in ('Y', 'y'):
+            for data in self.item.values():
+                del data[:]
 
+            print("Semua transaksi telah dihapus")
+            print(tabulate(self.item, headers='keys', tablefmt='github'))
 
     def total_price(self):
+        '''
+        Displaying total price.
+        Return: None
+        '''
         total = sum(self.item['Harga'])
-        
+
         if total > 400_000:
             total = total * (1 - 0.1)
         elif total > 300_000:
             total = total * (1 - 0.08)
         elif total > 200_000:
             total = total * (1 - 0.05)
-        
+
         print(f"Harga total: {total}")
-                    
+
     def check_order(self):
-        # untuk mengecek data jml item dan harga item valid atau tidak
-        # dilakukan operasi matematika, misal: penjumlahan
+        # TODO: gimana cara implementasi fungsi ini
+        '''
+        <enaknya diisi apa ya?>
+        '''
         try:
             total_jml_item = sum(self.item['Jumlah'])
             total_harga = sum(self.item['Harga'])
@@ -97,10 +151,3 @@ class Transaction:
 
         except Exception as exc:
             raise ValueError("Terdapat kesalahan input data") from exc
-
-# tsx_123 = Transaction()
-
-# tsx_123.add_item('Roti', 1, 7000)
-# tsx_123.update_item_name('Roti', 'Sari Roti')
-# print(tsx_123.item)
-
