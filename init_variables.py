db578a0a8cc654df9cd877266154e4164ba827f9
Rahman Aziz <rahmanazizf@gmail.com>
2023-01-17 17:25:25 +0700

    hapus file-file yang tidak digunakan

--- init_variables.py
+++
@@ -1,5 +0,0 @@
-# definisikan variable untuk mengakses database
-user = 'root'
-host = 'localhost'
-password = 'Fisikaituasyik123'
-dbname = 'inventori'
--- start.py
+++
@@ -1,2 +0,0 @@
-# modul untuk membuat database ketika pertama kali menjalankan program
-# modul ini hanya perlu dijalankan sekali ketika pertama kali pakai sebelum menjalankan main.py
